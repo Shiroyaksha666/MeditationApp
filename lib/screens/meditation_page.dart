4728c31151e39cce32a846b08cceff2f8eb11aca
Shiroyaksha666 <aliesmkhani00@gmail.com>
2024-10-18 15:52:20 +0330

    pages done

--- lib/screens/meditation_page.dart
+++ lib/screens/meditation_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:meditation_app/constant/consts.dart';
 import 'package:meditation_app/widgets/widgets.dart';
 
@@ -9,6 +10,31 @@ class MeditationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
+      bottomNavigationBar: Container(
+        height: 80,
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+        color: Colors.white,
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            NavigationIconMaker(
+              title: 'امروز',
+              svgsrc: 'assets/icons/calendar.svg',
+              isActive: false,
+            ),
+            NavigationIconMaker(
+              title: 'تمرینات',
+              svgsrc: 'assets/icons/gym.svg',
+              isActive: true,
+            ),
+            NavigationIconMaker(
+              title: 'تنظیمات',
+              svgsrc: 'assets/icons/Settings.svg',
+              isActive: false,
+            ),
+          ],
+        ),
+      ),
       body: Stack(
         children: <Widget>[
           Container(
@@ -92,83 +118,65 @@ class MeditationPage extends StatelessWidget {
                       ),
                     ],
                   ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'پیشنهاد ما',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 20),
+                    padding: const EdgeInsets.all(10),
+                    height: 90,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: const [
+                        BoxShadow(
+                          offset: Offset(0, 17),
+                          blurRadius: 23,
+                          spreadRadius: -13,
+                          color: kShadowColor,
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        SvgPicture.asset('assets/icons/Lock.svg'),
+                        const Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: <Widget>[
+                              Text(
+                                'یوگا پیشرفته',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w600),
+                              ),
+                              Text(
+                                'پیشرفته تر از قبل تمرین کنید',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w400),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        SvgPicture.asset(
+                            'assets/icons/Meditation_women_small.svg')
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
           )
         ],
       ),
-    );
-  }
-}
-
-class BuildSeseionCard extends StatelessWidget {
-  final String sesionNumber;
-  final bool isDone;
-  final VoidCallback? press;
-  const BuildSeseionCard({
-    super.key,
-    this.isDone = false,
-    required this.sesionNumber,
-    this.press,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(13),
-          child: Container(
-            width: constraints.maxWidth / 2 - 10,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(13),
-              boxShadow: const [
-                BoxShadow(
-                  offset: Offset(0, 17),
-                  blurRadius: 23,
-                  spreadRadius: -13,
-                  color: kShadowColor,
-                ),
-              ],
-            ),
-            child: Material(
-              color: Colors.transparent,
-              child: InkWell(
-                onTap: press,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Text(
-                        sesionNumber,
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Container(
-                        height: 42,
-                        width: 42,
-                        decoration: BoxDecoration(
-                            color: isDone ? kBlueColor : Colors.white,
-                            shape: BoxShape.circle,
-                            border: Border.all(color: kBlueColor)),
-                        child: Icon(
-                          Icons.play_arrow,
-                          color: isDone ? Colors.white : kBlueColor,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        );
-      },
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -111,3 +111,74 @@ class SearchBarMaker extends StatelessWidget {
     );
   }
 }
+
+class BuildSeseionCard extends StatelessWidget {
+  final String sesionNumber;
+  final bool isDone;
+  final VoidCallback? press;
+  const BuildSeseionCard({
+    super.key,
+    this.isDone = false,
+    required this.sesionNumber,
+    this.press,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(13),
+          child: Container(
+            width: constraints.maxWidth / 2 - 10,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(13),
+              boxShadow: const [
+                BoxShadow(
+                  offset: Offset(0, 17),
+                  blurRadius: 23,
+                  spreadRadius: -13,
+                  color: kShadowColor,
+                ),
+              ],
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: press,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        sesionNumber,
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 42,
+                        width: 42,
+                        decoration: BoxDecoration(
+                            color: isDone ? kBlueColor : Colors.white,
+                            shape: BoxShape.circle,
+                            border: Border.all(color: kBlueColor)),
+                        child: Icon(
+                          Icons.play_arrow,
+                          color: isDone ? Colors.white : kBlueColor,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
